78261451aa6a53db3cd764d88eef0c6cb1c1f82e
Noviana Ramadhani <novianarahmadani3@gmail.com>
2023-12-05 15:25:11 +0800

    Progres1

---
+++ app/src/main/java/com/example/register/booking/FormActivity.kt
@@ -0,0 +1,12 @@
+package com.example.register.booking
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.register.R
+
+class FormActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_form)
+    }
+}
